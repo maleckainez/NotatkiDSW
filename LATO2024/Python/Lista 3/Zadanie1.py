2424de8ab4d3b65125d3f7ebbc108a796ed5e0db
Inez Małecka <99558290+maleckainez@users.noreply.github.com>
2024-05-12 19:12:13 +0200

    Python 3/1 3/2 updt

--- LATO2024/Python/Lista 3/Zadanie1.py
+++ LATO2024/Python/Lista 3/Zadanie1.py
@@ -1,5 +1,4 @@
 #1. Napisz funkcję generatora, która generuje liczby parzyste z podanego zakresu.
-
 def generator():
     a = int(input("Proszę podaj wartość początkową generatora: "))
     b = int(input("Proszę podaj wartość końcową generatora:"))
