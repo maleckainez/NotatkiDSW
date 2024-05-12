4c3b87c831eac78c5a79b22fb92c8297d3f1f524
Inez Małecka <99558290+maleckainez@users.noreply.github.com>
2024-05-12 18:09:15 +0200

    python 2/4 updt

--- LATO2024/Python/Lista 2/Zadanie4.py
+++ LATO2024/Python/Lista 2/Zadanie4.py
@@ -1,1 +1,14 @@
 #Zadanie Nr - "Coś tam o loginie, nie mam motywacji...."
+def sprawdz_login(login):
+    # Sprawdź, czy login zawiera tylko dozwolone znaki (litery, cyfry, podkreślenia)
+    if not login.isalnum() or "_" in login:
+        raise ValueError("Niepoprawny login. Login może zawierać tylko litery, cyfry i podkreślenia.")
+    else:
+        print("Login jest poprawny!")
+
+# Przykład użycia:
+try:
+    login_usera = input("Podaj login: ")
+    sprawdz_login(login_usera)
+except ValueError as e:
+    print(f"Błąd: {e}")
