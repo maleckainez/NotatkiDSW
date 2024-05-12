0a522653d8043516376410674c81bdee8d480b0c
Inez Małecka <99558290+maleckainez@users.noreply.github.com>
2024-05-12 17:45:01 +0200

    python 2/3 updt

---
+++ LATO2024/Python/Lista 2/Zadanie3.py
@@ -0,0 +1,12 @@
+#Zadanie Nr9 - "Napisz program w Pythonie, który prosi użytkownika o podanie dwóch liczb całkowitych i wykonuje ich potęgowanie...."
+def read_text_file(filename):
+    try:
+        with open(filename, 'r') as file:
+            content = file.read()
+            print(f"Zawartość pliku {filename}:\n{content}")
+    except FileNotFoundError:
+        print(f"Plik {filename} nie został znaleziony.")
+
+# Przykład użycia
+read_text_file("moj_plik.txt")
+read_text_file("tekst_do_zad3.txt")
