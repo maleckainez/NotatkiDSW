fec8ad02c6ca4fe5fd0773e7deed82344ac45208
Inez Małecka <99558290+maleckainez@users.noreply.github.com>
2024-05-13 19:56:54 +0200

    Python 5/5 added

---
+++ LATO2024/Python/Lista 5/Zadanie 5.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+wektor1 = np.array([1, 2, 3])
+wektor2 = np.array([4, 5, 6])
+
+iloczyn_skalarny = np.dot(wektor1, wektor2)
+
+# Wyświetlenie wyniku
+print("Iloczyn skalarny:", iloczyn_skalarny)
