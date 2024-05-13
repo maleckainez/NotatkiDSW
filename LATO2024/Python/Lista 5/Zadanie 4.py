77fee6c4ee25660724107b7201ca42a11b4571e0
Inez Małecka <99558290+maleckainez@users.noreply.github.com>
2024-05-13 19:55:35 +0200

    Python 5/4 added

---
+++ LATO2024/Python/Lista 5/Zadanie 4.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+# Tworzenie tablicy numpy z liczbami całkowitymi od 1 do 100
+tablica = np.arange(1, 101)
+
+# Znajdowanie minimum i maksimum w tablicy
+minimum = np.min(tablica)
+maksimum = np.max(tablica)
+
+# Wyświetlanie wyników
+print(tablica)
+print(minimum)
+print(maksimum)
