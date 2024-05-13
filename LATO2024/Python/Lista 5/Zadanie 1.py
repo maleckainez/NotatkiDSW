f3f33546a1a2a70f0f6159d23143357af81db21f
Inez Małecka <99558290+maleckainez@users.noreply.github.com>
2024-05-13 19:27:29 +0200

    Python 5/1 added

---
+++ LATO2024/Python/Lista 5/Zadanie 1.py
@@ -0,0 +1,12 @@
+import numpy
+
+array1 = numpy.array([[1, 2, 3],
+                   [4, 5, 6],
+                   [7, 8, 9]])
+
+array2 = numpy.array([[9, 8, 7],
+                   [6, 5, 4],
+                   [3, 2, 1]])
+result = array1 + array2
+print("Wynik dodawania:")
+print(result)
