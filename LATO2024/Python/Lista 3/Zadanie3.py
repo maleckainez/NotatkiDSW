791b252b03f5f857614c7d7c4fb7ec60c4294027
Inez Małecka <99558290+maleckainez@users.noreply.github.com>
2024-06-23 15:40:04 +0200

    Python 3/3 popr

--- LATO2024/Python/Lista 3/Zadanie3.py
+++ LATO2024/Python/Lista 3/Zadanie3.py
@@ -2,29 +2,21 @@ def zadanie3():
     parzyste = []
     nieparzyste = []
 
-    def generator_listy_zlozonej():
-        podstawa = []
-        kwadrat = []
-        szescian = []
-        for i in range(1, 11):
-            podstawa.append(i)
-            kwadrat.append(i ** 2)
-            szescian.append(i ** 3)
-        lista_scalona = podstawa + kwadrat + szescian
-        return lista_scalona
+    def generator_dla_listy():
+        for i in range(1, 21):
+            yield i, i**2, i**3
 
-    def segregacja_liczb(lista_scalona):
-        for liczba in lista_scalona:
-            if liczba % 2 == 0:
-                parzyste.append(liczba)
-            else:
-                nieparzyste.append(liczba)
+    def segregacja_liczb(generator):
+        for krotka in generator():
+            for liczba in krotka:
+                if liczba % 2 == 0:
+                    parzyste.append(liczba)
+                else:
+                    nieparzyste.append(liczba)
 
-    lista_scalona = generator_listy_zlozonej()
-    segregacja_liczb(lista_scalona)
+    segregacja_liczb(generator_dla_listy)
     print(f"Lista, zawierająca liczby parzyste: {parzyste}")
     print(f"Lista, zawierająca liczby nieparzyste: {nieparzyste}")
-
 
 if __name__ == '__main__':
     zadanie3()
