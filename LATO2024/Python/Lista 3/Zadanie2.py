e78d2286344d8e96f16f2abf01d91a90d7221810
Inez Małecka <99558290+maleckainez@users.noreply.github.com>
2024-06-23 14:47:31 +0200

    Python 2/2 popr

--- LATO2024/Python/Lista 3/Zadanie2.py
+++ LATO2024/Python/Lista 3/Zadanie2.py
@@ -1,14 +1,15 @@
 # lista złożona
 def main():
-    podstawa = []
-    kwadrat = []
-    szescian = []
-    for i in range(1,11):
-        podstawa.append(i)
-        kwadrat.append(i**2)
-        szescian.append(i**3)
-    for i in range(len(podstawa)):
-        print(podstawa[i], kwadrat[i], szescian[i])
+    podstawa = [i for i in range(1,11)]
+    kwadrat = [i**2 for i in range(1,11)]
+    szescian = [i**3 for i in range(1,11)]
+    listapoteg = [(podstawa[x], kwadrat[x], szescian[x]) for x in range(len(podstawa))]
+    print("Printowanie listy w brzydkiej wersji:")
+    for krotka in listapoteg:
+        print(krotka)
+    print("\nPrintowanie listy w ładnej wersji:")
+    for x in podstawa:
+        print(podstawa[x], kwadrat[x], szescian[x])
 
 if __name__ == "__main__":
     main()
